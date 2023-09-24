ee4973ed1f75820ab1c9198f0e5753e925349d74
Piyanshu Saini <priyanshusaininew@gmail.com>
2023-09-24 17:09:48 +0530

    Add image to hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,20 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div></div>
-      <div></div>
+    <main className="flex relative">
+      <div className='bg-white w-1/2 h-screen'>
+        <span>App Developer</span>
+      </div>
+      <Image 
+        src='/img/priyanshusaini.png'
+        width={400}
+        height={400}
+        alt='Priyanshu'
+        className='w-[28rem] h-[28rem] absolute left-1/2 transform -translate-x-1/2 bottom-0'
+      />
+      <div className='bg-sky-950 w-1/2'>
+        <span className=''>App Developer</span>
+      </div>
     </main>
   )
 }
