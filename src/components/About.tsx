89ef76f2f426987b776db4dc9fa6a5954a48ff5d
PRIYANSHU SAINI <priyanshusaininew@gmail.com>
2023-09-29 06:56:31 +0000

    Add circle ton the hero sectioon

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,6 +4,6 @@ import React from 'react'
 
 export const About = () => {
   return (
-    <div>About</div>
+    <div className='h-[170rem]'>About</div>
   )
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,35 +1,43 @@
 "use client";
 import React, { useRef, useState } from "react";
 import Image from "next/image";
-import { motion } from "framer-motion";
+import { motion, useScroll,useSpring } from "framer-motion";
 
 interface Props {}
 
 export const Hero: React.FC = () => {
+  const imgRef = useRef<HTMLImageElement>(null);
+  const [imgPosition, setImgPostion] = useState<{
+    x: number | number[] | string;
+    y: number | number[];
+  }>({
+    x: "-50%",
+    y: 0,
+  });
 
-  const imgRef=useRef<HTMLImageElement>(null);
-  const [imgPosition,setImgPostion]=useState<{x:number|number[]|string,y:number|number[]}>({
-    x:'-50%',y:0
-  })
-
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
   // move img to the below with frammer animation
 
-  const moveImage=()=>{
-    setImgPostion(prev=>{
-      if(prev.x==0||prev.y==0){
+  const moveImage = () => {
+    setImgPostion((prev) => {
+      if (prev.x == 0 || prev.y == 0) {
         return {
-          x:-500,
-          y:[500]
-        }
-      }else{
+          x: -500,
+          y: [500],
+        };
+      } else {
         return {
-          x:'-50%',
-          y:0
-        }
+          x: "-50%",
+          y: 0,
+        };
       }
-    })
-  }
-
+    });
+  };
 
   return (
     <section className="flex relative w-full">
@@ -41,7 +49,12 @@ export const Hero: React.FC = () => {
         className="bg-white w-1/2 h-screen flex items-center justify-center"
       >
         <div className="text-center">
-          <h1 className="text-6xl text-gray-800 font-bold" onClick={()=>moveImage()}>App Developer</h1>
+          <h1
+            className="text-6xl text-gray-800 font-bold"
+            onClick={() => moveImage()}
+          >
+            App Developer
+          </h1>
           <p className="mt-4 text-xl text-gray-600">
             Turning Ideas into Reality
           </p>
@@ -49,10 +62,11 @@ export const Hero: React.FC = () => {
       </motion.div>
 
       {/* Image */}
-      <motion.div className="absolute left-1/2 transform bottom-0"
+      <motion.div
+        className="absolute left-1/2 transform bottom-0"
         animate={imgPosition}
-        transition={{duration:1.5,type:'spring',}}
-        initial={{x:'-50%'}}
+        transition={{ duration: 1.5, type: "spring" }}
+        initial={{ x: "-50%" }}
       >
         <Image
           src="/img/priyanshusaini.png"
@@ -62,8 +76,10 @@ export const Hero: React.FC = () => {
           className="w-[28rem] h-[28rem]"
           ref={imgRef}
         />
+      <motion.div className="bg-pink-500 w-screen h-[100vw] fixed  rounded-full mx-auto top-[3rem]"  style={{ scaleX,scaleY:scaleX }}>
       </motion.div>
-
+      
+      </motion.div>
       {/* Right Background */}
       <motion.div
         initial={{ opacity: 0, x: 100 }}
@@ -73,8 +89,12 @@ export const Hero: React.FC = () => {
       >
         <div className="text-center -rotate-90 text-start flex flex-col font-yatra">
           <h1 className="text-6xl text-white font-bold">App Developer</h1>
-          <h1 className="text-6xl text-zinc-400 opacity-70 font-bold">Web Developer</h1>
-          <h1 className="text-6xl text-neutral-600 font-bold">Meta Developer</h1>
+          <h1 className="text-6xl text-zinc-400 opacity-70 font-bold">
+            Web Developer
+          </h1>
+          <h1 className="text-6xl text-neutral-600 font-bold">
+            Meta Developer
+          </h1>
         </div>
       </motion.div>
     </section>
