c8e84a3b0612040c7cf83f907a838d8cf4c0cb4d
Piyanshu Saini <priyanshusaininew@gmail.com>
2023-10-14 23:01:00 +0530

    Create basic hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { About, Hero,BgVideo } from "@/components";
+import Hero2 from '../components/Hero2';
 
 
 export default function Home() {
   return (
     <main className="">
-      <BgVideo/>
+      <Hero2/>
+      {/* <Laptop/> */}
+      {/* <BgVideo/> */}
       {/* <Hero/>
       <About/> */}
     </main>
