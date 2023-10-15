e29dc09aca18ac3f8e703c518717c609d5da7b23
PRIYANSHU SAINI <priyanshusaininew@gmail.com>
2023-10-15 10:16:27 +0000

    Create CTO Button Animation

--- src/components/Hero2.tsx
+++ src/components/Hero2.tsx
@@ -5,10 +5,32 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Hero2 = () => {
+  const div1Variants = {
+    initial: { scaleX: 1, scaleY: 1 },
+    hover: { scaleX: 1.03, scaleY: 0.98 },
+  };
+
+  const span1Variants = {
+    initial: { x: 0 },
+    hover: { x: "100%", transition: { delay: 0.3 } }, // Translate to the left with a delay
+  };
+
+  const span2Variants = {
+    initial: { x: 0 },
+    hover: { x: "100%", transition: { delay: 0.5 } }, // Translate to the left with a delay
+  };
+
+  const wobbleVariants = {
+    hover: {
+      x: [0, -10, 10, -10, 10, 0], // The x values to create a wobble effect
+      transition: { type: "spring", stiffness: 200, damping: 10 },
+    },
+  };
+
   return (
     <section className="relative h-screen bg-[#fafafa] flex flex-col gap-5 justify-center pt-64">
-      <div className="flex flex-col justify-center items-center gap-7 text-center">
-        <h1 className="text-6xl text-center font-bold font-mont">
+      <div className="flex flex-col justify-center items-center gap-8 text-center justify-center">
+        <h1 className="text-6xl text-center font-bold font-mont mb-8">
           Code <br />
           that Speaks
         </h1>
@@ -17,15 +39,43 @@ const Hero2 = () => {
           <br /> to Amplify User Satisfaction with Intuitive Interfaces
         </p>
 
-        <Link href="#explore" className="rounded-full bg-[#8330C2] text-white flex p-6 px-8 gap-4 items-center justify-around">
-          <span className="text-4xl uppercase font-mont">Explore </span>
-          <Image
-            src='/icons/arrow-right.svg'
-            width={24}
-            height={24}
-            alt='Arrow right'
-            className="invert"
-            />
+        <Link href="#explore" className="rounded-full">
+          <motion.div
+            className="rounded-full bg-[#8330C2] overflow-hidden relative"
+            variants={div1Variants}
+            whileHover="hover"
+          >
+            <motion.span
+              className="absolute w-full h-full bg-purple-600 -left-full block rounded-full"
+              variants={span1Variants}
+            >
+              <motion.div
+                className="w-full h-full"
+                variants={wobbleVariants}
+                whileHover="hover"
+              ></motion.div>
+            </motion.span>
+            <motion.span
+              className="absolute w-full h-full bg-purple-500 -left-full block rounded-full"
+              variants={span2Variants}
+            >
+              <motion.div
+                className="w-full h-full"
+                variants={wobbleVariants}
+                whileHover="hover"
+              ></motion.div>
+            </motion.span>
+            <span className="text-white flex p-6 px-10 gap-4 items-center justify-around z-50 relative">
+              <span className="text-3xl uppercase font-mont">Explore </span>
+              <Image
+                src="/icons/arrow-right.svg"
+                width={24}
+                height={24}
+                alt="Arrow right"
+                className="invert"
+              />
+            </span>
+          </motion.div>
         </Link>
       </div>
       <Image
