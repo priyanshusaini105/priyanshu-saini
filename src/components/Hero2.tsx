a82ca0b4c7bd3ef506b5768eb29e590f54893dc0
priyanshusaini1 <123226128+priyanshusaini1@users.noreply.github.com>
2023-10-15 05:54:53 +0000

    Create CTO button

--- src/components/Hero2.tsx
+++ src/components/Hero2.tsx
@@ -12,11 +12,47 @@ const Hero2 = () => {
           Code <br />
           that Speaks
         </h1>
-        <p className="text-2xl">Simplify App and Web Development<br/> to Amplify User Satisfaction with Intuitive Interfaces</p>
+        <p className="text-2xl">
+          Simplify App and Web Development
+          <br /> to Amplify User Satisfaction with Intuitive Interfaces
+        </p>
 
-        <Link href='#explore'>
-            <span>Explore <svg xmlnsXlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" className="pt-svgsprite -arrow-right-o" width={32} height={32}><defs><symbol viewBox="0 0 32 32" id="arrow-right-o" xmlns="http://www.w3.org/2000/svg"><path d="m18 5 12 11m0 0L18 27m12-11H2" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" /></symbol></defs><g><path d="m18 5 12 11m0 0L18 27m12-11H2" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" /></g></svg>
-</span>
+        <Link href="#explore" className="rounded-full bg-[#8330C2] text-white flex p-6 px-8 gap-4 items-center justify-around">
+          <span className="text-4xl uppercase">Explore </span>
+          <svg
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            xmlns="http://www.w3.org/2000/svg"
+            className="pt-svgsprite -arrow-right-o"
+            width={32}
+            height={32}
+          >
+            <defs>
+              <symbol
+                viewBox="0 0 32 32"
+                id="arrow-right-o"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="m18 5 12 11m0 0L18 27m12-11H2"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2.5"
+                />
+              </symbol>
+            </defs>
+            <g>
+              <path
+                d="m18 5 12 11m0 0L18 27m12-11H2"
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2.5"
+              />
+            </g>
+          </svg>
         </Link>
       </div>
       <Image
