a4c1b6c456d479a24d5d097c4c199431ffa48f07
priyanshusaini1 <123226128+priyanshusaini1@users.noreply.github.com>
2023-10-15 05:46:35 +0000

    Create Hero text

--- src/components/Hero2.tsx
+++ src/components/Hero2.tsx
@@ -2,24 +2,31 @@
 import { motion } from "framer-motion";
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Hero2 = () => {
   return (
-    <div className="relative h-screen bg-white">
-      <div className="flex justify-center items-center">
-        <h1 className="text-6xl text-center">
-          Welcome <br />
-          to My portfolio
+    <section className="relative h-screen bg-[#fafafa] flex flex-col gap-5 justify-center pt-64">
+      <div className="flex flex-col justify-center items-center gap-7 text-center">
+        <h1 className="text-6xl text-center font-bold">
+          Code <br />
+          that Speaks
         </h1>
+        <p className="text-2xl">Simplify App and Web Development<br/> to Amplify User Satisfaction with Intuitive Interfaces</p>
+
+        <Link href='#explore'>
+            <span>Explore <svg xmlnsXlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" className="pt-svgsprite -arrow-right-o" width={32} height={32}><defs><symbol viewBox="0 0 32 32" id="arrow-right-o" xmlns="http://www.w3.org/2000/svg"><path d="m18 5 12 11m0 0L18 27m12-11H2" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" /></symbol></defs><g><path d="m18 5 12 11m0 0L18 27m12-11H2" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" /></g></svg>
+</span>
+        </Link>
       </div>
       <Image
         src="/img/priyanshusaini.png"
-        width={200}
-        height={200}
+        width={400}
+        height={400}
         alt="Priyanshu Saini"
         className=""
       />
-    </div>
+    </section>
   );
 };
 
